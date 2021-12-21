6fd2ab42a6334a2a12f57d2732b622e41a82dd7e
Lurid <epticid@gmail.com>
2021-12-21 11:47:21 +0600

    Add files via upload

--- site/js/script.js
+++ site/js/script.js
@@ -15,7 +15,42 @@ function removeAllChildNodes(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
+
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    // при необходимости добавьте другие значения по умолчанию
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+}
+
+
 
 { // variables
 
@@ -165,9 +200,16 @@ document.addEventListener("DOMContentLoaded", function(){
 	//SF.list[0].value = true;
 	//console.log("SF.row[0][0] = " + SF.row[0][1].value);
 
-	let date = new Date(Date.now() + 86400e3);
-	date = date.toUTCString();
-	document.cookie = "user=John;expires=" + date;
+	//let date = new Date(Date.now() + 86400e3);
+	//date = date.toUTCString();
+	//document.cookie = "user=John; expires=" + date;
+
+	let in_c = getCookie("in_count");
+
+	in_c++;
+
+	setCookie("in_count", in_c);
+
 	console.log("cookie=(" + document.cookie + ")");
 
 	new_game();
